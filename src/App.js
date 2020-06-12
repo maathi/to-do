e64d87aa876bea29d0f106c4505c54b48e5f08d5
mehdi <mahdi221b@gmail.com>
2020-06-12 13:59:39 +0000

    starter

--- src/App.js
+++ src/App.js
@@ -2,25 +2,55 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+let items = ["milk", "sugar", "gun"]
+let tmp = "hihihihj"
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form></Form>
+      <List></List>
     </div>
   );
+}
+
+class Form extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <h3>i am the form</h3>
+        <input></input>
+        <button onClick={addMe}>add</button>
+      </div>
+    )
+  }
+}
+
+
+function addMe(){
+  console.log("hello")
+}
+let x = <h1>how about now</h1>
+
+class List extends React.Component{
+  render(){
+    return(
+      <div>
+       <h6>and i'm the list</h6> 
+        {
+          items.map((item)=> 
+            <li key={item.toString()}>
+              {item}
+              <button>X</button>
+            </li>
+          )
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
